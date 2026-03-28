1e0cf959c7c6419412a9a5d9f4d6718e5c477573
Ivan Francisco <hansivann23@gmail.com>
2026-03-28 01:10:37 -0700

    in progress: orders/create webhook with db logic

--- app/routes/webhooks.app.orders_create.jsx
+++ app/routes/webhooks.app.orders_create.jsx
@@ -1,19 +1,21 @@
-import {authenticate} from "../shopify.server"
+import {authenticate} from "../shopify.server";
+import db from '../db.server';
 
 export const action = async ({request}) => {
   const {payload, session, topic, shop} = await authenticate.webhook(request);
 
-  const orderId = payload.id;
+  const shopifyOrderId = payload.id;
   const firstName = payload.customer.first_name;
   const lastName = payload.customer.last_name;
-  const email = payload.customer.email;
+  const customerName = `${firstName} ${lastName}`;
+  const customerEmail = payload.customer.email;
   const lineItems = payload.line_items;
 
   const vendors = Object.groupBy(lineItems, (lineItem) => {
     return lineItem.vendor;
   })
 
-  const groubByVendor = lineItems.reduce(
+  const groupByVendor = lineItems.reduce(
     (accumulator, lineItem) => {
       const key = lineItem.vendor;
 
@@ -25,4 +27,40 @@ export const action = async ({request}) => {
 
       return accumulator;
     }, {})
-  }
+
+    console.log(`Received ${topic} webhook for ${shop}`);
+
+    if(session) {
+      await db.order.create({
+        data: {
+          shopifyOrderId,
+          customerName,
+          customerEmail,
+          shop,
+    }})
+
+      Object.keys(groupByVendor).map((vendor) => {
+        const lineItems = groupByVendor[vendor];
+
+
+        lineItems.map((lineItem) => {
+          db.lineItem.create({
+            data: {
+              shopifyLineItemId: lineItem.id,
+              title: lineItem.title,
+              quantity: lineItem.quantity,
+              price: lineItem.price,
+              vendor,
+              order: {
+                connect: {
+                  shopifyOrderId
+                }
+              }
+            }
+          })
+        })
+      }})
+    }
+
+
+}
